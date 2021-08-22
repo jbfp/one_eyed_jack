d7ff756f02b29f8788cb693811c94f0cf9516cac
jbfp <jakob@jbfp.dk>
2021-08-22 10:20:05 +0200

    Remove service worker

--- src/wwwroot/src/index.tsx
+++ src/wwwroot/src/index.tsx
@@ -7,15 +7,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
     <BrowserRouter>
         <App />
     </BrowserRouter>
 ), document.getElementById('root'));
-
-serviceWorker.register();
 
 // Global unhandled error event handler:
 const LOG_URL = `logs`;
